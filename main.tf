1f135f30183b01a84751d5e2b3fdf57f60ee17e0
Charles Brown <225094140+Brownc81@users.noreply.github.com>
2026-04-06 19:13:44 -0400

    fix: remediate all Snyk IaC findings 04/06/2026

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "aws_s3_bucket" "insecure_bucket" {
 resource "aws_s3_bucket_public_access_block" "insecure_bucket" {
   bucket = aws_s3_bucket.insecure_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true # SNYK-CC-TF-95
+  block_public_policy     = true #SNYK-CC-TF-96
+  ignore_public_acls      = true # SNYK-CC-TF-95
+  restrict_public_buckets = true #SNYK-CC-TF-98 04.06
 }
 
 resource "aws_security_group" "wide_open" {
@@ -18,7 +18,7 @@ resource "aws_security_group" "wide_open" {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"] #snyk-cc-tf-1 04/06/2026 
   }
 }
 
@@ -29,8 +29,8 @@ resource "aws_iam_policy" "wildcard_policy" {
     Version = "2012-10-17"
     Statement = [{
       Effect   = "Allow"
-      Action   = "*"
-      Resource = "*"
+      Action   = "s3:ListBucket" # SNYK-CC-TF-69 04/06 "*"Allows every single AWS action
+      Resource = "arn:aws:s3:::insecure-demo-bucket"             # "*"Applies to every AWS resource
     }]
   })
 }
@@ -40,8 +40,21 @@ resource "aws_instance" "no_imdsv2" {
   instance_type = "t2.micro"
 
   metadata_options {
-    http_tokens = "optional"
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true    # ← add this
   }
 }
+
+# resource "aws_instance" "no_imdsv2" {
+#   ami           = "ami-0c55b159cbfafe1f0"
+#   instance_type = "t2.micro"
+
+#   metadata_options {
+#     http_tokens = "optional"
+#   }
+# }
 
 
